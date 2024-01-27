0e4bd06abe630eba76620c2456c24f4d30fcdf28
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-01-27 16:34:08 -0500

    create static section 1 of homepage

--- src/components/homepage/section1.js
+++ src/components/homepage/section1.js
@@ -1,11 +1,16 @@
 import style from "./section1.module.css";
+import fullMushroom from "../../assets/2D/Mushroom Full Piece.png";
+import Image from "next/image";
 
 function Section1() {
   return (
     <div className={style.container}>
+      <div className={style.bgImage}>
+        <Image src={fullMushroom} alt="Mushroom"/>
+      </div>
       <div className={style.name}>Ignite</div>
       <div className={style.tedx}>TEDxCMU 2024: Ignite</div>
-      <div className={style.datetime}>February</div>
+      <div className={style.datetime}>February 24, 7:00 PM EST<br></br>Location: UC McConomy</div>
     </div>
   );
 }
