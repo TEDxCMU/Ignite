a68a3e2cecc8506ea9d3cab1ed3912e6434d4c99
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-01-27 17:06:24 -0500

    add homepage section 2 animation

--- src/components/homepage/section2.js
+++ src/components/homepage/section2.js
@@ -1,9 +1,31 @@
+import { useEffect, useRef } from "react";
 import style from "./section2.module.css";
+import { animate } from "motion";
 
 function Section2() {
+
+  const blurbRef = useRef(null)
+
+  useEffect(() => {
+    if (!blurbRef.current) return;
+
+    animate(
+      `.blurb`,
+      { x: [-blurbRef.current.offsetWidth-50, 0] },
+      {
+        duration: 3,
+        repeat: Infinity,
+        easing: 'linear'
+      }
+    );
+  }, [blurbRef]);
+
   return (
     <div className={style.container}>
-      Where does it all begin?
+      <div className={`blurb ${style.blurb}`} ref={blurbRef}>Where does it all <span className={style.begin}>begin?</span></div>
+      <div className={`blurb ${style.blurb}`}>Where does it all <span className={style.begin}>begin?</span></div>
+      <div className={`blurb ${style.blurb}`}>Where does it all <span className={style.begin}>begin?</span></div>
+      <div className={`blurb ${style.blurb}`}>Where does it all <span className={style.begin}>begin?</span></div>
     </div>
   );
 }
