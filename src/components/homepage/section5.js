2eb4991ff6ee57a6baad7a445c6d142f9660340e
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-01-27 22:14:57 -0500

    added section five

--- src/components/homepage/section5.js
+++ src/components/homepage/section5.js
@@ -1,10 +1,17 @@
 import style from "./section5.module.css";
+import ignites from "../../assets/2D/It-Ignites.png";
+import Image from "next/image";
 
 function Section5() {
   return (
     <div>
       <div className={style.container1}></div>
-      <div className={style.container2}>It ignites.</div>
+      <div className={style.container2}>
+        <div className={style.itIgnites}>
+          <div>It</div>
+          <Image src={ignites} alt="Ignites"/>
+        </div>
+      </div>
     </div>
   );
 }
