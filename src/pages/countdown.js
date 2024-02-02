3884cdb64ad7fa2008a89af1993ec74ceefdc8e1
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-02 17:34:47 -0500

    clean up old code

--- src/pages/countdown.js
+++ src/pages/countdown.js
@@ -56,17 +56,3 @@ function CountdownGames() {
 // };
 
 export default CountdownGames;
-
-// function Countdown() {
-//   return (
-//     <div>
-//       <Link href="/countdown/p5GameDemo">p5.js Demo</Link>
-//       <Link href="/countdown/3DPhysicsDemo">3D Physics Demo</Link>
-//       <Link href="/countdown/3DAnimationDemo">3D Animation Demo</Link>
-//       <Link href="/countdown/2DDemo">2D Demo with Physics</Link>
-//       <Leaderboard />
-//     </div>
-//   );
-// }
-
-// export default Countdown;
