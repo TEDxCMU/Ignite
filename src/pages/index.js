6672b2aefc847631d10b898242b3af396473a259
Ethan Huang <yehuang2020@gmail.com>
2024-02-02 11:46:23 -0500

    removed section 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Section2 from "components/homepage/section2";
 import Section3 from "components/homepage/section3";
 import Section4 from "components/homepage/section4";
 import Section5 from "components/homepage/section5";
-import Section6 from "components/homepage/section6";
 
 function Home() {
   return (
