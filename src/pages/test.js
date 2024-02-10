c855f9339e267e45e44715a7dfa8f97e2b599e46
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-09 19:00:14 -0500

    make a janky but working distortion effect

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import DistortionEffect from "components/homepage/distortionEffect";
+
+function Stage() {
+  return (
+      <div>
+        <div>test</div>
+        <DistortionEffect />
+      </div>
+  );
+}
+
+export default Stage;
