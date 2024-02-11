248eee3529c81e2f881b786f57baf921c238df50
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-11 15:44:02 -0500

    fix section 3 for mobile

--- src/components/homepage/section3.js
+++ src/components/homepage/section3.js
@@ -1,5 +1,6 @@
 import style from "./section3.module.css";
 import Section4 from "./section4";
+import cn from "classnames";
 
 function Section3() {
   return (
@@ -10,15 +11,15 @@ function Section3() {
         </div>
         <div className={style.text}>
           <div className={style.blurbStart}>With an</div>
-          <div className={style.blurbWord}>idea</div>
+          <div className={style.blurbWord}>idea,</div>
           <div className={style.blurbText}>Verb that means to set something on fire or to cause it to start burning. It is the action of initiating a combustion process, typically involving a spark, heat, or flame. Igniting something can also be used metaphorically to describe the act of starting or triggering a process, event, or emotion. Igniting something can also be used metaphorically to describe the act of starting or triggering a process, event, or emotion.</div>
         </div>
       </div>
       
-      <div className={style.wrap}>
+      <div className={style.wrap2}>
         <div className={style.text}>
           <div className={style.blurbStart}>Starting from an</div>
-          <div className={style.blurbWord}>origin</div>
+          <div className={style.blurbWord}>origin.</div>
           <div className={style.blurbText}>Verb that means to set something on fire or to cause it to start burning. It is the action of initiating a combustion process, typically involving a spark, heat, or flame. Igniting something can also be used metaphorically to describe the act of starting or triggering a process, event, or emotion. Igniting something can also be used metaphorically to describe the act of starting or triggering a process, event, or emotion.</div>
         </div>
         <div className={style.video}>
