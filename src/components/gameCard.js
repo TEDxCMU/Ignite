b573a6e23107e44f0032f8ad8decb22b441e185d
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-11 23:57:33 -0500

    make countdown page good for mobile

--- src/components/gameCard.js
+++ src/components/gameCard.js
@@ -4,29 +4,40 @@ import Image from "next/image";
 import filler from "../assets/filler1.png";
 import Link from "next/link";
 
+import cn from "classnames";
+
 export default function GameCard(props) {
     const { game, locked } = props;
 
     return (
-        <div className={styles.container}>
+        <>
             {locked ? 
-            <div>
-                <div className={styles.day}>{game.daysleft}</div>
-                <div className={styles.daysleft}>day{game.daysleft > 1 && "s"} left</div>
+            <div className={cn(styles.container, styles.containerLocked)}>
+                <div className={styles.daycount}>
+                    <div className={styles.day}>{game.daysleft}</div>
+                    <div className={styles.daysleft}>day{game.daysleft > 1 && "s"} left</div>
+                </div> 
             </div> :
-            <Link 
-                href={game.link.url}
-            >
-                <Image
-                    className={styles.image}
-                    style={{ objectFit: "contain", padding: "7% 5%" }}
-                    src={filler}
-                />
-                <div className={styles.info}>
-                    <div className={styles.name}>{game.name}</div>
-                </div>
-            </Link>
-            }
-        </div>
+            <div className={styles.container}>
+                <Link 
+                    className={styles.link}
+                    href={game.link.url}
+                >
+                    <Image
+                        className={styles.image}
+                        width={0}
+                        height={0}
+                        sizes="30vw"
+                        style={{ objectFit: "cover"}}
+                        src={game.image.url}
+                    />
+                    <div className={styles.info}>
+                        <div className={styles.name}>{game.name}</div>
+                        <div className={styles.body}>a game about xyz made by abc :) wahhoohohoho</div>
+                    </div>
+                </Link>
+            </div>
+        }
+    </>
     );
 }
