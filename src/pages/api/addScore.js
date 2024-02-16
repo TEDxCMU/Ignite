7f4fdf094482de59ec6166ca88a230174bfdc6b8
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-16 02:24:30 -0500

    cors

--- src/pages/api/addScore.js
+++ src/pages/api/addScore.js
@@ -1,10 +1,19 @@
 // pages/api/addScore.js
+import nextCors from 'next-cors';
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function addScore(req, res) {
+    // Run the CORS middleware
+    await nextCors(req, res, {
+        // Options here, such as which origins to allow
+        methods: ['GET', 'POST', 'OPTIONS'],
+        origin: '*', // Adjust this to restrict to specific origins as needed
+        optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
     if (req.method === 'POST') {
         try {
             await client.connect();
@@ -15,7 +24,8 @@ async function addScore(req, res) {
             await client.close();
         }
     } else {
-        res.status(405).end(); // Method Not Allowed
+        // If the request uses a method we don't support, return 405 Method Not Allowed
+        res.status(405).end();
     }
 }
 
--- src/pages/api/getScores.js
+++ src/pages/api/getScores.js
@@ -1,17 +1,31 @@
 // pages/api/getScores.js
+import nextCors from 'next-cors';
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function getScores(req, res) {
-    try {
-        await client.connect();
-        const collection = client.db("leaderboard-database").collection("leaderboard-collection");
-        const scores = await collection.find({}).sort({ score: -1 }).toArray();
-        res.status(200).json(scores);
-    } finally {
-        await client.close();
+    // Run the CORS middleware
+    await nextCors(req, res, {
+        // Options here, such as which origins to allow
+        methods: ['GET', 'POST', 'OPTIONS'],
+        origin: '*', // Adjust this to restrict to specific origins as needed
+        optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
+    if (req.method === 'GET') {
+        try {
+            await client.connect();
+            const collection = client.db("leaderboard-database").collection("leaderboard-collection");
+            const scores = await collection.find({}).sort({ score: -1 }).toArray();
+            res.status(200).json(scores);
+        } finally {
+            await client.close();
+        }
+    } else {
+        // If the request uses a method we don't support, return 405 Method Not Allowed
+        res.status(405).end();
     }
 }
 
