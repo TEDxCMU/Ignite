ef2a2cc8bf39786f0cdb55766f7857f5e650b152
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2024-02-17 17:10:33 -0700

    firework sketch compiles now

--- src/components/fireworksgame/fireworks.js
+++ src/components/fireworksgame/fireworks.js
@@ -37,34 +37,43 @@ const sketch = (p5) => {
     "Fiery",
     "Illuminate"
   ];
+  let allSprites = [];
+
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
+
   p5.setup = () => {
     p5.createCanvas(800, 600);
-    spr = createSprite(
+    spr = new Sprite(
       600, 500, 40, 40);
-    spr.shapeColor = color(255);
+    spr.shapeColor = p5.color(255);
     spr.velocity.y = -2;
-    spr.text = arr[getRandomInt(0, 25)];
+    spr.text = arr[getRandomInt(0, 25)].toLowerCase();
     spr.textSize = 20;
+    allSprites.push(spr);
   }
+
   p5.draw = () => {
     p5.background(50);
     p5.fill("white");
     p5.textSize(25);
     p5.text("Score: " + score, 15, 25);
-    if (millis() >= 2000+timer) {
-      spr = createSprite(getRandomInt(100, 760), 500, 40, 40);
-      spr.shapeColor = color(255);
+    if (p5.millis() >= 2000+timer) {
+      p5.print("new sprite created")
+      spr = new Sprite(getRandomInt(100, 760), 500, 40, 40);
+      spr.shapeColor = p5.color(255);
       spr.velocity.y = -2;
-      spr.text = arr[getRandomInt(0, 25)];
+      spr.text = arr[getRandomInt(0, 25)].toLowerCase();
       spr.textSize = 20;
-      timer = millis();
+      allSprites.push(spr);
+      timer = p5.millis();
     }
     for (let i = 0; i < allSprites.length; i++) {
+      allSprites[i].update();
+      allSprites[i].draw();
       if (allSprites[i].position.y < 0) {
         p5.noLoop();
         p5.fill("red");
@@ -72,12 +81,12 @@ const sketch = (p5) => {
         p5.text("Game Over", 280, 300);
       }
     }
-    drawSprites();
+
   }
 
   p5.keyTyped = () => {
     //contents = contents + key;
-    k = key;
+    k = p5.key;
     if(currTyping){
       if(k == curr.text[0]){
         curr.text = curr.text.substring(1);
@@ -94,13 +103,49 @@ const sketch = (p5) => {
           currTyping = true;
           curr = allSprites[i];
           curr.text = curr.text.substring(1);
-          curr.shapeColor = color(120);
+          curr.shapeColor = p5.color(120);
             score += 1;
           return;
         }
       }
     }
     k = "";
+  }
+
+  class Sprite {
+    constructor(x, y, w, h) {
+      this.position = {
+        x: x,
+        y: y
+      };
+      this.w = w;
+      this.h = h;
+
+      this.shapeColor = p5.color(255);
+      this.velocity = {
+        x: 0,
+        y: 0
+      };
+      this.text = "";
+      this.textSize = 20;
+    }
+
+    update() {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+
+    draw() {
+      p5.fill(this.shapeColor);
+      p5.rect(this.position.x, this.position.y, this.w, this.h);
+      p5.fill("white");
+      p5.textSize(this.textSize);
+      p5.text(this.text, this.position.x, this.position.y);
+    }
+
+    remove() {
+      allSprites.splice(allSprites.indexOf(this), 1);
+    }
   }
 };
   
